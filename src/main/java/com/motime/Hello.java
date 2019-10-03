406ae7735bfa019cc821bf6a080519f8c219e734
Nemo Chen <495k0056@stust.edu.tw>
2019-10-03 15:48:24 +0800

    Initial commit

---
+++ src/main/java/com/motime/Hello.java
@@ -0,0 +1,9 @@
+package com.motime;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.printf("Hello word");
+        Person p = new Person();
+        p.hello();
+    }
+}
